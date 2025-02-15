1059394b653bf2f33e645712367b10319314322b
Hieu Vu <hieuvu@Hieus-MacBook-Pro.local>
2025-02-16 03:10:52 +0700

    feat: add user repo

---
+++ server/repo/user.repo.ts
@@ -0,0 +1,33 @@
+import { equal } from "assert";
+import $prisma from "../lib/prisma";
+
+async function createUser(input: { email: string; pwd: string }) {
+  return $prisma.user.create({
+    data: {
+      email: input.email,
+      password: input.pwd,
+    },
+  });
+}
+
+async function getUserByEmail(email: string) {
+  return $prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+}
+
+async function getUserById(id: string) {
+  return $prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+export const TodoRepo = {
+  createUser,
+  getUserByEmail,
+  getUserById,
+};
