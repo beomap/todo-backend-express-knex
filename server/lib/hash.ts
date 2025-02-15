a91169e402b117eceb36291f03d6707353a7cb6a
Hieu Vu <hieuvu@Hieus-MacBook-Pro.local>
2025-02-16 03:28:52 +0700

    feat: add jwt utils

--- server/lib/hash.ts
+++ server/lib/hash.ts
@@ -1,4 +1,5 @@
-import bcrypt, { compare } from "bcrypt";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const saltRounds = 10;
 
@@ -9,4 +10,23 @@ export async function hashPassword(pwd: string) {
 
 export async function comparePassword(pwd: string, hashedPwd: string) {
   return bcrypt.compare(pwd, hashedPwd);
+}
+
+export async function signJWT(payload: any, secret: string) {
+  return jwt.sign(payload, secret, {
+    expiresIn: "1h",
+  });
+}
+
+export async function verifyJWT(token: string, secret: string) {
+  const isOk = await jwt.verify(token, secret);
+  if (isOk) {
+    return decodeJWT(token);
+  }
+
+  throw Error("invalid token");
+}
+
+export async function decodeJWT(token: string) {
+  return jwt.decode(token);
 }
